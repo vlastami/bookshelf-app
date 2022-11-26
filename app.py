5c9d9aef8188f8612d91e7574c604ad448658b81
Vlasta Rencova <rencovav@seznam.cz>
2022-11-26 12:23:35 +0100

    pridano html

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+db = dict()
+
 @app.route('/')
 def uvodni_stranka():
-    return '<h1>Muj NoSQL projekt</h1>'
+    return render_template("index.html", databaze=db)
+
+@app.route("/kontakt", methods=["GET", "POST"])
+def kontakt():
+    if request.method == "GET":
+        return render_template("kontakt.html")
+    elif request.method == "POST":
+        email = request.form["email"]
+        dotaz = request.form["dotaz"]
+        db[email] = dotaz
+
+        return render_template("kontakt.html", vzkaz="Dotaz byl nahran")
 
 if __name__ == "__main__":
     app.run(debug=True)
