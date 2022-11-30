a95b112a145e9a1f2702a63fbd52e9f2b88e3e1e
Vlasta Rencova <rencovav@seznam.cz>
2022-11-30 17:48:28 +0100

    MongoDB connection, books and authors

--- app.py
+++ app.py
@@ -1,23 +1,65 @@
+import datetime
+
 from flask import Flask, render_template, request
+
+from mongoengine import *
 
 app = Flask(__name__)
 
-db = dict()
+db = connect(host="mongodb://localhost:27017/my_db")
+
+
+class Author(Document):
+    name = StringField(required=True, max_length=70)
+    birthDate = StringField()
+    country = StringField(required=True, max_length=70)
+    biography = StringField(required=True, max_length=300)
+
+
+class Book(Document):
+    name = StringField(required=True, max_length=70)
+    publication = StringField(required=True)
+    author = ReferenceField(Author)
+    genres = ListField(StringField)
+    # dateAdded = DateTimeField(required=True,default=datetime.datetime)
+
+
+authors = dict()
+
 
 @app.route('/')
-def uvodni_stranka():
-    return render_template("index.html", databaze=db)
+def mainpage():
+    return render_template("index.html", Authors=Author.objects, Books=Book.objects)
 
-@app.route("/kontakt", methods=["GET", "POST"])
-def kontakt():
+
+@app.route("/author", methods=["GET", "POST"])
+def author():
     if request.method == "GET":
-        return render_template("kontakt.html")
+        return render_template("author.html")
     elif request.method == "POST":
-        email = request.form["email"]
-        dotaz = request.form["dotaz"]
-        db[email] = dotaz
+        name = request.form["name"]
+        birth_date = request.form["birth_date"]
+        country = request.form["country"]
+        bio = request.form["bio"]
 
-        return render_template("kontakt.html", vzkaz="Dotaz byl nahran")
+        author = Author(name=name, birthDate=birth_date, country=country, biography=bio).save()
+
+        return render_template("autor.html", message="Author added")
+
+
+@app.route("/book", methods=["GET", "POST"])
+def book():
+    if request.method == "GET":
+        return render_template("book.html", Authors=Author.objects)
+    elif request.method == "POST":
+        name = request.form["name"]
+        publication = request.form["publication"]
+        author = request.form["author"]
+        genres = request.form["genres"]
+
+        book = Book(name=name, publication=publication, author=author, genres=genres).save()
+        return render_template("book.html", message="Book added")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
